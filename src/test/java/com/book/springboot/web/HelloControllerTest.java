ee90e9a3719bc7daffd33932f39104440e38494f
HongYeseul <ghd8119@naver.com>
2024-01-09 17:54:09 +0900

    :white_check_mark: test(HelloController): 롬복을 이용해 파라미터를 사용한 기능 테스트 해보기 - ~77 페이지

--- src/test/java/com/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/book/springboot/web/HelloControllerTest.java
@@ -7,8 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class) // 스프링 부트 테스트와 JUnit 사이의 연결자 역할
@@ -25,5 +27,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청
                 .andExpect(status().isOk()) // HTTP header의 status 검증
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto").param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
