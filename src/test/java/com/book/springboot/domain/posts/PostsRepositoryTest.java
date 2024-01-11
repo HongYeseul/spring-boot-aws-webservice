a2a5fc34bb471314f356e58e341c1636b595fa74
HongYeseul <ghd8119@naver.com>
2024-01-11 10:53:44 +0900

    :white_check_mark: test(PostsRepository): JPA를 추가한 코드 테스트 추가하기 - dialect = org.hibernate.dialect.MySQL5InnoDBDialect 설정은 굳이 하지 않았음 - 96-100 페이지

---
+++ src/test/java/com/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.book.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest // 별다른 설정 없이 사용시 H2 데이터베이스를 자동 실행
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() //save는 id 값이 있다면 update, 없다면 insert
+                .title(title)
+                .content(content)
+                .author("yeseul")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
