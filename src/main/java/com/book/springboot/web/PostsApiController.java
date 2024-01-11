1d5187e741304acc0019a23d7858e2e94fe749c6
HongYeseul <ghd8119@naver.com>
2024-01-11 11:24:52 +0900

    :white_check_mark: feat(PostsSave): 게시글 save 기능 추가 - requestDto/controller/service - 104-108 페이지

---
+++ src/main/java/com/book/springboot/web/PostsApiController.java
@@ -0,0 +1,24 @@
+package com.book.springboot.web;
+
+import com.book.springboot.service.posts.PostsService;
+import com.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring에서 Bean을 주입 받는 방식에는 @Autowired, setter, 생성자를 사용하는 방식의 총 3가지가 있다.
+ * 이 중 가장 권장하는 방식이 생성자로 주입받는 방식이다.
+ * @RequiredArgsConstructor 의 경우 자동으로 생성자를 생성해주기 때문에 간단하게 해결할 수 있다.
+ */
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
