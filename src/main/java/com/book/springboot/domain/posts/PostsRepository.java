e6d23778b9d5ae4be2318c64c940c8720a1d5731
HongYeseul <ghd8119@naver.com>
2024-01-12 22:47:52 +0900

    :sparkles: feat(posts.save): 메인화면 구성 및 게시글 저장 기능 개발 - 메인화면에 저장한 모든 게시글 조회 - 125-150 페이지

--- src/main/java/com/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/book/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,15 @@
 package com.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // DB Layer 접근자
     // JpaRepository<Entity 클래스, PK 타입>을 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
     // 중요! Entity 클래스와 기본 Entity Repository 는 함께 위치해야한다.
+
+    @Query("SELECT  p FROM Posts  p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.book.springboot.service.posts;
 
 import com.book.springboot.domain.posts.Posts;
 import com.book.springboot.domain.posts.PostsRepository;
+import com.book.springboot.web.dto.PostsListResponseDto;
 import com.book.springboot.web.dto.PostsResponseDto;
 import com.book.springboot.web.dto.PostsSaveRequestDto;
 import com.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +42,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // readOnly: 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도 개션
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
