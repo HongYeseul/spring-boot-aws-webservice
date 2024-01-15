fe48a33640a53829109d53d035ede93bdb20b658
HongYeseul <ghd8119@naver.com>
2024-01-15 18:22:15 +0900

    :recycle: feat(LoginUser): 어노테이션 기반으로 개션하기 - 로그인 세션 정보를 가지고올 수 있는 LoginUser 만들기 - 195-199 페이지

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.book.springboot.web;
 
+import com.book.springboot.config.auth.LoginUser;
 import com.book.springboot.config.auth.dto.SessionUser;
 import com.book.springboot.service.posts.PostsService;
 import com.book.springboot.web.dto.PostsResponseDto;
@@ -18,13 +19,14 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) { // 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 됨
         // Model: 서버 템플린 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
         // 여기서는 postsService.findAllDesc()로 가져온 결과를 Posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
 
         // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 했고, 그 값을 가져옴
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
